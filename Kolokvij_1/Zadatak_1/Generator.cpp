ee01e730b507e82efbc3e36af8a3a1bcd958e28c
vdimicpmfst <vdimic@pmfst.hr>
2022-12-02 09:40:48 +0100

    Kolokvij1_zad1

--- Kolokvij_1/Zadatak_1/Generator.cpp
+++ Kolokvij_1/Zadatak_1/Generator.cpp
@@ -9,15 +9,19 @@ void Generator::GeneratePDF(){
 	i = 0;
 	canvas = new TCanvas ();
 	hist = new TH1F ("GeneratePDF", "GeneratePDF", 48, -3., 9.);
+	int j=0;
 	while (i < 1E5){
 		rng = -3 + 12 * (float)rand () / RAND_MAX;
 		f = (-rng * rng + 6 * rng + 27) / 36.;
-		rng2 = -3 + 12 * rand () /RAND_MAX;
+		rng2 = -3 + 12 * (float)rand () /RAND_MAX;
 		if (rng2 < f){
 			hist -> Fill (rng);
 			i++;
 		}
+//	j++;
 	}
+//	cout << j << endl;
+//	cout << i << endl;
 	gPad -> SetLeftMargin (0.15);
 	gPad -> SetBottomMargin (0.15);
 	canvas -> SetCanvasSize (900, 900);
