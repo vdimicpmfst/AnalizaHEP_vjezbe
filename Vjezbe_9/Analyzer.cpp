e201a4f4b0bea1cd95ee883b4e27fa77545bb1fd
vdimicpmfst <vdimic@pmfst.hr>
2023-02-05 18:03:14 +0100

    vjezba9

--- Vjezbe_9/Analyzer.cpp
+++ Vjezbe_9/Analyzer.cpp
@@ -35,6 +35,28 @@ void Analyzer::LifetimeFit(){
 	HistLifetime -> GetYaxis () -> SetTitle ("N");
 	HistLifetime -> Draw ();
 	c1 -> Print ("LifetimeFit.pdf");
+	
+	cout << "Root: tau = " << exp -> GetParameter(1) << "±" << exp -> GetParError(1) << endl;
+	
+	mean = sum / nentries;
+	error = mean / sqrt(nentries);
+	C = exp -> GetParameter(1);
+	//analiticki dobiven iz likelihood funkcije	
+	cout << "Analiticki: tau = " << mean << "±" << error << endl;
 
+	delete exp; 
 
+	//likelihood
+	
+	Likelihood = new TF1 ("Likelihood", "([0] / x) * exp(-1.0 / x)", 0.1, 10.0);
+	Likelihood -> SetParameter (0, C);
+	c2 = new TCanvas ();
+	c2 -> SetCanvasSize(600, 400);
+	Likelihood -> GetXaxis() -> SetTitle ("#tau");
+	Likelihood -> GetYaxis() -> SetTitle ("L(#tau)");
+	Likelihood -> SetTitle ("Likelihood za t = 1s");
+	Likelihood -> Draw();
+	c2 -> Print("Likelihood.pdf");
+
+	delete Likelihood;
 }
