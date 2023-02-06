2745cd3119ce0612756f6d9557b68daaeabe78bf
vdimicpmfst <vdimic@pmfst.hr>
2023-02-06 22:41:22 +0100

    Vjezbe13

--- Vjezbe_13/Analyzer.cpp
+++ Vjezbe_13/Analyzer.cpp
@@ -37,20 +37,22 @@ void Analyzer::GenerateTestStatistic_H0(){
 	double sum;
 	
 	for (int i = 0; i < 1000000; i++){
-		for (int j = 0, sum = 0; j < 100; j++)
+		sum = 0.;
+		for (int j = 0; j < 100; j++)
 			sum += rng -> Gaus(164.7, 7.1);
 		testStatistic_PDF -> Fill(sum / 100.);
+			//cout << sum  << endl; 
 	}
 	
-	canvas -> cd();
-/*	testStatistic_PDF -> SetFillColor(kRed - 2);
+//	canvas -> cd();
+	testStatistic_PDF -> SetFillColor(kRed - 2);
 	testStatistic_PDF -> SetLineColor(kRed - 2);
 	
 	testStatistic_PDF -> SetTitle("Test Statistic PDF distribution");
 	testStatistic_PDF -> GetXaxis() -> SetTitle("#bar{h} [cm]");
 
 	testStatistic_PDF -> Scale(1. / testStatistic_PDF -> Integral());
-*/	testStatistic_PDF -> Draw("HIST");
+	testStatistic_PDF -> Draw("HIST");
 	
 	canvas -> Print("test.pdf");
 
